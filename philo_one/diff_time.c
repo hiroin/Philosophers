2c69584ee1a6a1f079f6a76415f722ded51e19ed
hiroin <egamescape@gmail.com>
2020-11-18 18:31:59 +0900

    fix norm

--- philo_one/diff_time.c
+++ philo_one/diff_time.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 02:35:09 by user42            #+#    #+#             */
-/*   Updated: 2020/11/18 10:30:50 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/18 10:31:53 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ long			diff_time(struct timeval end, struct timeval start)
 	long	diff_time;
 
 	sec = end.tv_sec - start.tv_sec;
-	microsec = end.tv_usec / 1000 - start.tv_usec /1000;
+	microsec = end.tv_usec / 1000 - start.tv_usec / 1000;
 	diff_time = sec * 1000 + microsec;
 	return (diff_time);
 }
