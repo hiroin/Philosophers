5e8b1c3a8edb951262df9f6f4396e5265132bbb4
hiroin <egamescape@gmail.com>
2020-11-18 18:25:11 +0900

    fix norm

--- philo_two/diff_time.c
+++ philo_two/diff_time.c
@@ -6,15 +6,11 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 02:35:09 by user42            #+#    #+#             */
-/*   Updated: 2020/11/18 03:05:57 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/18 10:23:14 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
-
-/*
-** 時刻の差分をミリ秒で返す関数
-*/
 
 long			diff_time(struct timeval end, struct timeval start)
 {
@@ -23,7 +19,7 @@ long			diff_time(struct timeval end, struct timeval start)
 	long	diff_time;
 
 	sec = end.tv_sec - start.tv_sec;
-	microsec = end.tv_usec / 1000- start.tv_usec / 1000;
+	microsec = end.tv_usec / 1000 - start.tv_usec / 1000;
 	diff_time = sec * 1000 + microsec;
 	return (diff_time);
 }
